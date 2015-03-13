f6e0dba9b3e3c61f1eb8d4c5cb23d8ee1cb961bd
acemaster <vivekhtc25@gmail.com>
2015-03-13 18:32:10 +0530

    Commitiing

--- librarian/models.py
+++ librarian/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+	bookid=models.IntegerField(primary_key=True)
+	accno=models.IntegerField()
+	classno=models.CharField(max_length=100)
+	bookno=models.CharField(max_length=100)
+	name=models.CharField(max_length=100)
+	no_copies=models.IntegerField()
+	pubid=models.ForeignKey(Publisher)
+	def __unicode__(self):
+		return self.bookid+" "+self.bname
+
+class 
